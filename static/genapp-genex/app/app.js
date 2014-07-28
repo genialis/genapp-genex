4bba374664c1c04a2e40ee44bf1d8349ff61c52b
Miha Stajdohar <miha.stajdohar@gmail.com>
2014-07-28 16:56:25 -0500

    Added data table.

--- static/genapp-genex/app/app.js
+++ static/genapp-genex/app/app.js
@@ -1,10 +1,17 @@
 'use strict';
 
 
-angular.module('genex', ['ngRoute', 'genjs.services', 'genex.controllers'])
+angular.module('genex', ['ngRoute', 'ngGrid', 'genjs.services', 'genjs.table',
+    'genex.controllers', 'blueimp.fileupload'])
 
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/', {
+
+        $routeProvider
+        .when('/', {
+            templateUrl: '/static/genapp-genex/partials/genex.html',
+            controller: 'GenExCtrl'
+        })
+        .when('/:projectId/', {
             templateUrl: '/static/genapp-genex/partials/genex.html',
             controller: 'GenExCtrl'
         })
--- static/genapp-genex/app/controllers.js
+++ static/genapp-genex/app/controllers.js
@@ -2,7 +2,41 @@
 
 
 angular.module('genex.controllers', [])
-    .controller('GenExCtrl', ['$scope', function ($scope) {
+    .controller('GenExCtrl', ['$scope', '$route', 'notify', 'Project',
+        function ($scope, $route, notify, Project) {
 
+        var projectId = $route.current.params.projectId;
+
+        $scope.selection = [];
+
+        if (!projectId) {
+            notify({ type: 'error', message: 'No project ID found in the url address' });
+
+        } else {
+
+            Project.get({ id: $route.current.params.projectId }, function (proj) {
+                $scope.tableOptions.project = proj;
+            });
+        }
+
+        $scope.tableOptions = {
+            itemsByPage: 15,
+            project: null,
+            genId: 'txttable',
+            filter: function(obj) { return obj.type === 'data:genex:text:' },
+            multiSelect: false,
+            selectedItems: $scope.selection,
+            showImport: false,
+            showExport: false,
+            showDelete: false,
+            showShare: false
+        };
+
+        $scope.uploadOptions = {
+            url: '/upload/',
+            sequentialUploads: true,
+            autoUpload: true,
+            singleClickAccept: true
+        };
     }])
 ;
