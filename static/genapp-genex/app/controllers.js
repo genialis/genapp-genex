81c991ce472067058bd96b6c478b4142354383a6
lukaw3d <lukaw3d@gmail.com>
2014-09-07 21:15:59 +0200

    Fix file uploading and add file processing

--- static/genapp-genex/app/controllers.js
+++ static/genapp-genex/app/controllers.js
@@ -2,19 +2,19 @@
 
 
 angular.module('genex.controllers', [])
-    .controller('GenExCtrl', ['$scope', '$route', 'notify', 'Project',
-        function ($scope, $route, notify, Project) {
+    .controller('GenExCtrl', ['$scope', '$route', 'notify', 'Project', 'Data', 'DataCache', 'fileUpload', 'createAndWaitProcessor',
+        function ($scope, $route, notify, Project, Data, DataCache, fileUpload, createAndWaitProcessor) {
+
+        var uploadProcName = 'import:upload:textfile';
+        var uploadProcType = 'data:genex:text:';
+
+        var processProcName = 'genex:wordfreq';
 
         var projectId = $route.current.params.projectId;
-
-        $scope.selection = [];
-
         if (!projectId) {
             notify({ type: 'error', message: 'No project ID found in the url address' });
-
         } else {
-
-            Project.get({ id: $route.current.params.projectId }, function (proj) {
+            Project.get({ id: projectId }, function (proj) {
                 $scope.tableOptions.project = proj;
             });
         }
@@ -23,27 +23,50 @@ angular.module('genex.controllers', [])
             itemsByPage: 15,
             project: null,
             genId: 'txttable',
-            filter: function(obj) { return obj.type === 'data:genex:text:' },
+            filter: function (obj) { return obj.type === uploadProcType; },
             multiSelect: false,
-            selectedItems: $scope.selection,
+            selectedItems: [],
             showImport: false,
             showExport: false,
             showDelete: false,
             showShare: false
         };
 
-        $scope.uploadOptions = {
-            url: '/upload/',
-            sequentialUploads: true,
-            autoUpload: true,
-            singleClickAccept: true
-        };
+        $scope.uploadOptions = fileUpload.chunkRetrying({
+            finalDone: function (serverFile, clientFile) {
+                var process = new Data({
+                    case_id: projectId,
+                    processor_name: uploadProcName,
+                    input: {
+                        txtin: {
+                            file: serverFile.name,
+                            file_temp: serverFile.temp
+                        }
+                    }
+                });
+                DataCache.save(process);
+            }
+        });
 
-        $element.find('#fileupload').on('fileuploaddone', function (e, data) {
-            angular.forEach(data.result.files, function (file) {
-                console.log(file);
-                //runProcessor(file, $scope.options.processorName);
+        $scope.$watchCollection('tableOptions.selectedItems', function (items) {
+            if (_.isEmpty(items)) return;
+            $scope.item = items[0];
+
+            createAndWaitProcessor(projectId, processProcName, { txtin: $scope.item.id }, 1000, function (d) {
+                return d.output.freq; // TODO: where is storage ID saved
+            }).promise.then(function (data) {
+                $scope.results = data;
+            }, function (reason) {
+                console.log('error', reason);
+            }, function (data){
+                $scope.processingItem = data;
             });
         });
+
+        $scope.withoutSchemas = function (obj) {
+            return _.omit(obj, function (value, key) {
+                return _.contains(key, 'schema');
+            });
+        };
     }])
 ;
